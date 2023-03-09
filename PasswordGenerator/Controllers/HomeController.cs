841111ea2593b986f635356570b8a1c562102b6c
Hector Rodriguez <herector6@gmail.com>
2023-03-08 21:13:36 -0600

    Add project files.

---
+++ PasswordGenerator/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace PasswordGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string _validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()_+";
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(int length = 12)
+        {
+            var random = new Random();
+            var password = new string(Enumerable.Repeat(_validChars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+
+            ViewBag.Password = password;
+            return View();
+        }
+    }
+}
---
+++ PasswordGenerator/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace PasswordGenerator
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
